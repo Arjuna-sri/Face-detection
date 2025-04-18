02a1194a8a9b4707058abbccc9dc188d003c1abf
Arjuna-sri <arjunasri555@gmail.com>
2025-04-18 20:31:10 +0530

    Add files via upload

---
+++ face detection.py
@@ -0,0 +1,19 @@
+import cv2
+
+alg = "haarcascade_frontalface_default.xml"
+harr_cascade = cv2.CascadeClassifier(alg)
+cam = cv2.VideoCapture(0)
+
+while True:
+    _, img = cam.read()
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = harr_cascade.detectMultiScale(grayImg, 1.3, 4)
+    for (x, y, w, h) in face:
+        cv2.circle(img, (x + w//2, y + h//2), w//2, (255, 105, 180), 2) 
+        cv2.imshow("Face Detection", img)
+        key = cv2.waitKey(10)
+        if key == 27:
+            break
+
+cam.release()
+cv2.destroyAllWindows()
